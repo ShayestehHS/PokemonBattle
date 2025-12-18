723cc9ca70951510065bf8c7c4f536e119caf12d
shayestehhs <shayestehhs1@gmail.com>
2025-12-18 17:02:30 +0330

    refactor: remove utils module and UUIDv7Model

--- backend/utils/__init__.py
+++
@@ -1,6 +0,0 @@
-from utils.models import UUIDv7Model
-from utils.pagination import ScoreboardCursorPagination
-from utils.cache import PokemonCache
-
-__all__ = ['UUIDv7Model', 'ScoreboardCursorPagination', 'PokemonCache']
-
